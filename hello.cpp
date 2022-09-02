884271eb6e5f1b86584f9e4e4d37ad16777c85c1
lmx <2813175506@qq.com>
2022-09-03 07:03:48 +0800

    Added a new hello world cpp project

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+const int N = 1010;
+
+int main()
+{
+	int n; 
+	cout << "HELLO WORLD" << endl;
+	return 0;
+}
